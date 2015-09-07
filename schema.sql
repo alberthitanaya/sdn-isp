374aead6f18c24fb2f436876b88fdb8852e38d28
alberthitanaya <alberthitanaya@gmail.com>
2015-09-08 00:00:52 +1000

    changed database entries to remove hypens and underscores as it was screwing the URI up with chrome

--- schema.sql
+++ schema.sql
@@ -26,10 +26,10 @@ create table smp (
     primary key (id)
 );
 
-INSERT INTO customers VALUES ('john_home','00:00:b0:48:7a:db:46:04','4', 7, 750 );
-INSERT INTO customers VALUES ('albert_home', '00:00:f8:d1:11:39:4a:76', '4', 15, 200);
-INSERT INTO customers VALUES ('uniwide_sdn', '00:00:a0:36:9f:1e:0c:cf', '3', 30, 1000);
+INSERT INTO customers VALUES ('johnhome','00:00:b0:48:7a:db:46:04','4', 7, 750 );
+INSERT INTO customers VALUES ('alberthome', '00:00:f8:d1:11:39:4a:76', '4', 15, 200);
+INSERT INTO customers VALUES ('uniwidesdn', '00:00:a0:36:9f:1e:0c:cf', '3', 30, 1000);
 
-INSERT INTO users VALUES ('albert_1','john_home');
-INSERT INTO users VALUES ('albert_2','albert_home');
-INSERT INTO users VALUES ('albert_3','uniwide_sdn');
+#INSERT INTO users VALUES ('albert1','johnhome');
+#INSERT INTO users VALUES ('albert2','alberthome');
+#INSERT INTO users VALUES ('albert3','uniwidesdn');
